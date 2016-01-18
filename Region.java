19004955cd18d3b447b878fd0250c68eb8bb159e
WhatsitTooya <adamlhancock@yahoo.com>
2016-01-17 23:57:55 -0800

    Working on making regions

--- Region.java
+++ Region.java
@@ -1,8 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
 public class Region {
 	
 	public ArrayList<Zone> region;
 	public Biome biome;
 	public String name;
+	public int[] loc0;
 	
 	
 	public Region(Zone start){
@@ -12,12 +17,18 @@ public class Region {
 			start.setRegion(this);
 			this.region.add(start);
 			this.biome = start.biome;
-			
-			
+			this.loc0 = start.relLoc;
 			
 			
 		}
 	}
 	
-	
+	public defineRegion() {
+		int y = loc0[0];
+		int x = loc0[1];
+		Arraylist<Point> used = new ArrayList<Point>();
+		Point a = new Point(x,y);
+		used.add(a);
+		//iterate through all tiles using two arraylists to check where you are and have been
+	}
 }
