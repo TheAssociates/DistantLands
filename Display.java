0b61b4d777eb986a060093a3a75a4676c1aacf58
NamesAreAPain <username@users.noreply.github.com>
2016-01-20 16:28:26 -0800

    Call Me Dr.Frankenstein

--- Display.java
+++ Display.java
@@ -6,14 +6,14 @@ import java.util.Properties;
 
 public class Display{
 	
-	public static void main(String[]args){
-		
-		//makes the window
+	private Zone[][] worldMap;
+	
+	public Display(Zone[][] wM){
+		this.worldMap = wM;
 		Properties text = new Properties();
 		text.setProperty("fontSize","10");
 		text.setProperty("font", "DEFAULT");
-		ConsoleSystemInterface csi = null;
-		
+		WSwingConsoleInterface csi = null;
 		try{
 			csi = new WSwingConsoleInterface("COLOR TEST", text);
 		}
@@ -22,15 +22,34 @@ public class Display{
 			eiie.printStackTrace();
 			System.exit(-1);
 		}
-		
-		int a = 0;
-		int b = 0;
-		
-		csi.print(0,0,"appke",CSIColor.BLUE,CSIColor.WHITE);
-		
-		
-		
+	}
+ 	
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[]args){
+		//makes the window
+		Properties text = new Properties();
+		text.setProperty("fontSize","30");
+		text.setProperty("font", "DEFAULT");
+		WSwingConsoleInterface csi = null;
+		try{
+			csi = new WSwingConsoleInterface("COLOR TEST", text);
+		}
+		catch (ExceptionInInitializerError eiie) {
+			System.out.println("*** Error: Swing Console Box cannot be initialized. Exiting...");
+			eiie.printStackTrace();
+			System.exit(-1);
+		}
+		csi.print(0,0,"Call me Frankenstein, bitches",CSIColor.BLUE,CSIColor.WHITE);
 	}
 	
 	
